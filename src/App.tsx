ec524cedc3f2d0c464fb41a670703c8048f2613b
Eduardo Muchak <eduardomuchak@gmail.com>
2023-01-17 09:19:14 -0300

    feat: :tada: configuracao inicial projeto

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import Habit from './components/habit'
+
+function App() {
+
+  return (
+    <Habit />
+  )
+}
+
+export default App
---
+++ src/components/habit/index.tsx
@@ -0,0 +1,9 @@
+function Habit() {
+  return (
+    <div>
+      <h1>Habit</h1>
+    </div>
+  );
+}
+
+export default Habit;
