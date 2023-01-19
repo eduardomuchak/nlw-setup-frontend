f5fa38d7183e42fbd1ffa14e700952b7b4203be1
Eduardo Muchak <eduardomuchak@gmail.com>
2023-01-19 14:21:08 -0300

    feat: :sparkles: adiciona modal de criar novo habito
    wip: necessario adicionar checkbox dias da semana

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,5 @@
 import logo from '../../assets/logo.svg';
-import NewHabitButton from '../NewHabitButton';
+import NewHabitModal from '../NewHabitModal';
 import SummaryTable from '../SummaryTable';
 
 function Header() {
@@ -7,7 +7,7 @@ function Header() {
     <div className={'w-full max-w-5xl px-6 flex flex-col gap-16'}>
       <div className={'w-full max-w-3xl mx-auto flex items-center justify-between'}>
         <img src={logo} className="Habits logo" alt="Habits logo" />
-        <NewHabitButton />
+        <NewHabitModal />
       </div>
       <SummaryTable />
     </div>
---
+++ src/components/NewHabitModal/index.tsx
@@ -0,0 +1,29 @@
+import { Plus, X } from 'phosphor-react';
+import * as Dialog from '@radix-ui/react-dialog';
+import NewHabitForm from '../NewHabitForm';
+
+function NewHabitButton() {
+  return (
+    <Dialog.Root>
+      <Dialog.Trigger
+        type="button"
+        className="border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-600"
+      >
+        <Plus size={20} className="text-violet-500" />
+        Novo hábito
+      </Dialog.Trigger>
+      <Dialog.Portal>
+        <Dialog.Overlay className="w-screen h-screen bg-black/80 fixed inset-8" />
+        <Dialog.Content className="absolute p-10 bg-zinc-900 rounded-2xl w-full max-w-md top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+          <Dialog.Close className="absolute right-6 top-6 text-zinc-400 hover:text-zinc-200 transition-all duration-400">
+            <X size={24} aria-label="Botão Fechar Modal" />
+          </Dialog.Close>
+          <Dialog.Title className="text-3xl leading-tight font-extrabold">Criar Hábito</Dialog.Title>
+          <NewHabitForm />
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
+  );
+}
+
+export default NewHabitButton;
