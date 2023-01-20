5ae0485bd0be032cee9fd1c735b9d7db0d2be7c0
Eduardo Muchak <eduardomuchak@gmail.com>
2023-01-20 10:17:50 -0300

    feat: :sparkles: adiciona logica para selecionar e armazenar states para criar novo habito

--- src/components/CheckboxWeekDay/index.tsx
+++ src/components/CheckboxWeekDay/index.tsx
@@ -1,13 +1,13 @@
 import * as Checkbox from '@radix-ui/react-checkbox';
 import { Check } from 'phosphor-react';
 
-interface Props {
+interface Props extends React.ComponentProps<typeof Checkbox.Root> {
   weekDay: string;
 }
 
-export function CheckboxWeekDay({ weekDay }: Props) {
+export function CheckboxWeekDay({ weekDay, ...rest }: Props) {
   return (
-    <Checkbox.Root className="flex items-center gap-3 group">
+    <Checkbox.Root className="flex items-center gap-3 group" {...rest}>
       <div className="w-8 h-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-violet-500 group-data-[state=checked]:border-violet-500 transition-all duration-400">
         <Checkbox.Indicator>
           <Check size={20} color="white" />
--- src/components/NewHabitForm/index.tsx
+++ src/components/NewHabitForm/index.tsx
@@ -1,7 +1,13 @@
 import { Check } from 'phosphor-react';
 import { CheckboxWeekDay } from '../CheckboxWeekDay';
+import { useState } from 'react';
 
 function NewHabitForm() {
+  const [formValues, setFormValues] = useState({
+    title: '',
+    weekDays: [] as number[],
+  });
+
   const availableWeekDays = [
     'Domingo',
     'Segunda-feira',
@@ -12,17 +18,33 @@ function NewHabitForm() {
     'Sábado',
   ];
 
+  const createNewHabit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+
+  const handleToggleWeekDay = (weekDayIndex: number) => {
+    const alreadySelected = formValues.weekDays.includes(weekDayIndex);
+
+    if (alreadySelected) {
+      const filteredWeekDays = formValues.weekDays.filter((item) => item !== weekDayIndex);
+      setFormValues({ ...formValues, weekDays: filteredWeekDays });
+    } else {
+      setFormValues({ ...formValues, weekDays: [...formValues.weekDays, weekDayIndex] });
+    }
+  };
+
   return (
-    <form className="w-full flex flex-col mt-6">
+    <form onSubmit={(event) => createNewHabit(event)} className="w-full flex flex-col mt-6">
       <label htmlFor="title" className="font-semibold leading-tight">
         Qual o seu comprometimento?
       </label>
       <input
+        className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
         type="text"
         id="title"
         placeholder="ex.: Exercícios, dormir 8h, etc..."
         autoFocus
-        className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
+        onChange={(event) => setFormValues({ ...formValues, title: event.target.value })}
       />
 
       <label htmlFor="" className="font-semibold leading-tight mt-4">
@@ -31,7 +53,11 @@ function NewHabitForm() {
 
       <div className="flex flex-col gap-2 mt-3">
         {availableWeekDays.map((weekDay, index) => (
-          <CheckboxWeekDay key={`${index}-${weekDay}`} weekDay={weekDay} />
+          <CheckboxWeekDay
+            key={`${index}-${weekDay}`}
+            weekDay={weekDay}
+            onCheckedChange={() => handleToggleWeekDay(index)}
+          />
         ))}
       </div>
 
