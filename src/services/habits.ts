f746af30201fe620cf9bf75abd287f9cd1f3f7f4
Eduardo Muchak <eduardomuchak@gmail.com>
2023-01-23 21:04:03 -0300

    feat: :sparkles: adiciona servicos para listar todos os habitos, deletar e atualiza habito por id

--- src/services/habits.ts
+++ src/services/habits.ts
@@ -10,7 +10,22 @@ export const createHabit = async (payload: Habit) => {
   return data;
 };
 
+export const getHabits = async () => {
+  const { data } = await api.get('/habits');
+  return data;
+};
+
 export const updateToggleHabit = async (habitId: string) => {
   const { data } = await api.patch(`/habits/${habitId}/toggle`);
+  return data;
+};
+
+export const updateHabit = async (habitId: string, payload: Habit) => {
+  const { data } = await api.patch(`/habits/${habitId}`, payload);
+  return data;
+};
+
+export const deleteHabit = async (habitId: string) => {
+  const { data } = await api.delete(`/habits/${habitId}`);
   return data;
 };
