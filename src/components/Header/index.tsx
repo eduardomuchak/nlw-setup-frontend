230f5ff06fe3a04780c6c167e5f2f3ad8d8ea504
Eduardo Muchak <eduardomuchak@gmail.com>
2023-01-23 22:25:07 -0300

    feat: :sparkles: adiciona modal com lista de habitos e integracao backend deletar habito

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,17 @@
 import logo from '../../assets/logo.svg';
 import NewHabitModal from '../NewHabitModal';
 import SummaryTable from '../SummaryTable';
+import { ListOfHabitsModal } from '../ListOfHabitsModal';
 
 function Header() {
   return (
     <div className={'w-full max-w-5xl px-6 flex flex-col gap-16'}>
       <div className={'w-full max-w-3xl mx-auto flex items-center justify-between'}>
         <img src={logo} alt="Habits logo" />
-        <NewHabitModal />
+        <div className="flex flex-row gap-2">
+          <ListOfHabitsModal />
+          <NewHabitModal />
+        </div>
       </div>
       <SummaryTable />
     </div>
---
+++ src/components/ListOfHabitsModal/index.tsx
@@ -0,0 +1,85 @@
+import { List, Trash, X } from 'phosphor-react';
+import * as Dialog from '@radix-ui/react-dialog';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { deleteHabit, getHabits } from '../../services/habits';
+import { Loading } from '../Loading';
+import { RequestError } from '../RequestError';
+import { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
+
+interface Habit {
+  id: string;
+  title: string;
+  created_at: string;
+  weekDays: {
+    id: string;
+    habit_id: string;
+    week_day: number;
+  }[];
+}
+
+export function ListOfHabitsModal() {
+  const queryClient = useQueryClient();
+
+  const [allHabits, setAllHabits] = useState<Habit[]>([]);
+  const { isLoading, error, data: habits } = useQuery({ queryKey: ['habits'], queryFn: getHabits });
+
+  const mutation = useMutation({
+    mutationFn: deleteHabit,
+    onSuccess: () => {
+      toast.success('Hábito deletado com sucesso');
+      queryClient.invalidateQueries({ queryKey: ['habits'] });
+    },
+  });
+
+  const handleDeleteHabit = (id: string) => {
+    mutation.mutate(id);
+  };
+
+  useEffect(() => {
+    if (habits.length) setAllHabits(habits.sort((a: Habit, b: Habit) => (a.title > b.title ? 1 : -1)));
+  }, [habits]);
+
+  if (isLoading) return <Loading />;
+  if (error) return <RequestError />;
+
+  return (
+    <Dialog.Root>
+      <Dialog.Trigger
+        type="button"
+        className="border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-600 transition-colors focus:outline-none focus:ring-2 focus:ring-violet-600 focus:ring-offset-2 focus:ring-offset-background"
+      >
+        <List size={20} className="text-violet-500" />
+        Lista de Hábitos
+      </Dialog.Trigger>
+      <Dialog.Portal>
+        <Dialog.Overlay className="w-screen h-screen bg-black/80 fixed inset-8" />
+        <Dialog.Content className="absolute p-10 bg-zinc-900 rounded-2xl w-full max-w-md top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+          <Dialog.Close className="absolute right-6 top-6 text-zinc-400 rounded-md hover:text-zinc-200 transition-all duration-400 focus:outline-none focus:ring-2 focus:ring-violet-600 focus:ring-offset-2 focus:ring-offset-zinc-900">
+            <X size={24} aria-label="Botão Fechar Modal" />
+          </Dialog.Close>
+          <Dialog.Title className="text-3xl leading-tight font-extrabold">Lista de Hábitos</Dialog.Title>
+          <Dialog.Description className="text-xl mt-4 overflow-x-auto max-h-screen">
+            <ul className="mt-4">
+              {allHabits.map((habit) => (
+                <li key={habit.id} className="flex items-center justify-between mb-3">
+                  <span className="text-white">{habit.title}</span>
+                  <button
+                    className="group mr-2 p-1 rounded-lg hover:bg-red-500 hover:text-white focus:outline-none focus:ring-2 focus:ring-red-600 focus:ring-offset-2 focus:ring-offset-zinc-900 transition-all duration-300"
+                    onClick={() => handleDeleteHabit(habit.id)}
+                  >
+                    <Trash
+                      size={20}
+                      aria-label="Lixeira para deletar um hábito"
+                      className="text-red-500 group-hover:text-white transition-all duration-300"
+                    />
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </Dialog.Description>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
+  );
+}
