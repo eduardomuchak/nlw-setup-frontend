df9c231beb1590faadeff95281086e372929c689
Eduardo Muchak <eduardomuchak@gmail.com>
2023-01-26 20:59:20 -0300

    feat: :sparkles: adiciona pagina de login

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -6,20 +6,16 @@ export function NotFoundPage() {
   const navigate = useNavigate();
 
   return (
-    <PageContainer>
-      <div className={'flex items-center justify-center flex-col gap-4'}>
-        <img src={errorSVG} alt="404 Error" className="w-96 h-96" />
-        <h1 className="text-white text-3xl font-bold text-center">
-          Eita! Acho que a página que você buscou não existe!
-        </h1>
-        <button
-          className="mt-6 rounded-lg p-4 gap-3 flex items-center justify-center font-semibold bg-violet-600 hover:bg-violet-700 transition-all duration-400"
-          type="reset"
-          onClick={() => navigate('/')}
-        >
-          Voltar para página inicial
-        </button>
-      </div>
-    </PageContainer>
+    <div className={'h-screen w-screen flex items-center justify-center flex-col gap-4'}>
+      <img src={errorSVG} alt="404 Error" className="w-96 h-96" />
+      <h1 className="text-white text-3xl font-bold text-center">Eita! Acho que a página que você buscou não existe!</h1>
+      <button
+        className="mt-6 rounded-lg p-4 gap-3 flex items-center justify-center font-semibold bg-violet-600 hover:bg-violet-700 transition-all duration-400"
+        type="reset"
+        onClick={() => navigate('/')}
+      >
+        Voltar para página inicial
+      </button>
+    </div>
   );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,11 +2,13 @@ import { Routes, Route } from 'react-router-dom';
 import { SummaryPage } from '../pages/Summary';
 import { HabitsListPage } from '../pages/HabitsList';
 import { NotFoundPage } from '../pages/NotFound';
+import { LoginPage } from '../pages/Login';
 
 export function MainRoutes() {
   return (
     <Routes>
       <Route path="/" element={<SummaryPage />} />
+      <Route path="/login" element={<LoginPage />} />
       <Route path="/habits-list" element={<HabitsListPage />} />
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
