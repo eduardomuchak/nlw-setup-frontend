7db11a7165b12ca83d6f4a5363f0f6d5dda8c029
Eduardo Muchak <eduardomuchak@gmail.com>
2023-01-27 10:44:58 -0300

    feat: :sparkles: tabela de habitos em andamento

--- src/pages/HabitsList/index.tsx
+++ src/pages/HabitsList/index.tsx
@@ -1,21 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { PageContainer } from '../../components/PageContainer';
-import { useTable } from 'react-table';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { deleteHabit, getHabits } from '../../services/habits';
 import { toast } from 'react-toastify';
 import { RequestError } from '../../components/RequestError';
 import { Loading } from '../../components/Loading';
-import { Trash } from 'phosphor-react';
-
-const fakeData = [
-  {
-    id: 1,
-    habit: 'adsadsdas',
-    actions: 'asdasdasd',
-  },
-];
-
+import { Pencil, Trash } from 'phosphor-react';
 interface Habit {
   id: string;
   title: string;
@@ -31,6 +21,7 @@ export function HabitsListPage() {
   const queryClient = useQueryClient();
 
   const [allHabits, setAllHabits] = useState<Habit[]>([]);
+
   const { isLoading, error, data: habits } = useQuery({ queryKey: ['habits'], queryFn: getHabits });
 
   const header = React.useMemo(() => ['Hábito', 'Dias da Semana', 'Ações'], []);
@@ -71,41 +62,50 @@ export function HabitsListPage() {
   return (
     <PageContainer>
       <div className={'w-full max-w-5xl h-[88%] px-6 flex flex-col gap-16 flex-1 items-center justify-center mx-auto'}>
-        <table className="w-full w-max-5xl">
-          <thead className="w-full">
-            <tr className="flex w-full">
-              {header.map((column) => {
-                return <th className="flex-1">{column}</th>;
+        <table className="w-full w-max-5xl rounded-lg overflow-hidden">
+          <thead className="w-full bg-violet-500 rounded-lg">
+            <tr className="flex w-full py-4 px-6">
+              {header.map((column, index) => {
+                return (
+                  <th key={`${column}-${index}`} className={index === 2 ? 'flex-0' : 'flex-1'}>
+                    {column}
+                  </th>
+                );
               })}
             </tr>
           </thead>
           <tbody>
-            <tr>
-              {allHabits.map((habit) => {
-                return (
-                  <tr className="flex w-full">
-                    <td className="flex-1 text-center">{habit.title}</td>
-                    <td className="flex-1 text-center">
-                      {habit.weekDays.map((weekDay) => {
-                        return <span>{weekDay.week_day}</span>;
-                      })}
-                    </td>
-                    <td className="flex-1 text-center">
-                      <button
-                        className="group mr-2 p-1 rounded-lg hover:bg-red-500 hover:text-white focus:outline-none focus:ring-2 focus:ring-red-600 focus:ring-offset-2 focus:ring-offset-zinc-900 transition-all duration-300"
-                        onClick={() => handleDeleteHabit(habit.id)}
-                      >
-                        <Trash
-                          size={20}
-                          aria-label="Lixeira para deletar um hábito"
-                          className="text-red-500 group-hover:text-white transition-all duration-300"
-                        />
-                      </button>
-                    </td>
-                  </tr>
-                );
-              })}
-            </tr>
+            {allHabits.map((habit, index) => {
+              return (
+                <tr key={`${habit}-${index}`} className="flex w-full py-4 px-6">
+                  <td className="flex-1">{habit.title}</td>
+                  <td className="flex-1 text-center">
+                    {habit.weekDays.map((weekDay, index) => {
+                      return <span key={`${weekDay}-${index}`}>{weekDay.week_day}</span>;
+                    })}
+                  </td>
+                  <td className="flex-0 text-center">
+                    <button className="group p-1 rounded-lg hover:bg-violet-500 hover:text-white focus:outline-none focus:ring-2 focus:ring-violet-600 focus:ring-offset-2 focus:ring-offset-zinc-900 transition-all duration-300">
+                      <Pencil
+                        size={20}
+                        aria-label="Lápis para editar um hábito"
+                        className="text-violet-500 group-hover:text-white transition-all duration-300"
+                      />
+                    </button>
+                    <button
+                      className="group p-1 rounded-lg hover:bg-red-500 hover:text-white focus:outline-none focus:ring-2 focus:ring-red-600 focus:ring-offset-2 focus:ring-offset-zinc-900 transition-all duration-300"
+                      onClick={() => handleDeleteHabit(habit.id)}
+                    >
+                      <Trash
+                        size={20}
+                        aria-label="Lixeira para deletar um hábito"
+                        className="text-red-500 group-hover:text-white transition-all duration-300"
+                      />
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
           </tbody>
         </table>
       </div>
